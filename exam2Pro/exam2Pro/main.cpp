93d0b34509c3fb958409342f6a6a8e5de1470548
hengwuji <>
2022-05-04 17:40:50 +0800

    setup proj

---
+++ exam2Pro/exam2Pro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  exam2Pro
+//
+//  Created by tina_lias on 2022/5/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
